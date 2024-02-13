bb0a835a2f52614ac9f5eaeaaa18d542db6c8c00
Jason <murangira99@gmail.com>
2024-02-14 01:22:04 +0200

    added total interest

--- index.js
+++ index.js
@@ -15,3 +15,6 @@ function calculateResults(){
      const amount = document.getElementById('amount');
      const interest = document.getElementById('interest');
     const years = document.getElementById('years');
+    const monthlyPayment = document.getElementById('monthly-payment');
+    const totalPayment = document.getElementById('total-payment');
+    const totalInterest = document.getElementById('total-interest');
